96d8fd70cf2eacc105d155d88f8ad300093e27d5
maggobyte <magnust97@gmail.com>
2024-11-03 12:42:50 +0100

    V1

--- script.js
+++ script.js
@@ -8,13 +8,49 @@ inputField.addEventListener('keydown', function(event){
 });
 
 const replacements = {
-    'a':'b',
-    'b':'c'
+    'a':'4',
+    'b':'8',
+    'c':'C',
+    'd':'D',
+    'e':'3',
+    'f':'F',
+    'g':'6',
+    'h':'H',
+    'i':'1',
+    'j':'J',
+    'k':'K',
+    'l':'1',
+    'm':'M',
+    'n':'N',
+    'o':'Ø',
+    'p':'P',
+    'q':'Q',
+    'r':'R',
+    's':'5',
+    't':'7',
+    'u':'U',
+    'v':'V',
+    'w':'W',
+    'x':'X',
+    'y':'Y',
+    'z':'Z'
+
+
 }
 
 function translateText(){
-    const regex = new RegExp(Object.keys(replacements).join('|'), 'g');
+    const regex = new RegExp(Object.keys(replacements).join('|'), 'gi');
     const inputText = inputField.value;
-    const transformedText = inputText.replace(regex,(matched) => replacements[matched]);
+
+    const transformedText = inputText.replace(regex,(matched) => {
+        const lowerMatched = matched.toLowerCase();
+
+        if (lowerMatched in replacements){
+            return matched == matched.toUpperCase() ?
+                replacements[lowerMatched].toUpperCase() :
+                replacements[lowerMatched];
+        }
+        return matched;
+    });
     outputSpan.innerText = transformedText;
 }
